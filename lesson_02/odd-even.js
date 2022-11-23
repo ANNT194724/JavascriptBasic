8415c7b52fd265b8a7ca3b813c71e9c100769f50
NGUYEN TRUONG AN <an.nt194724@sis.hust.edu.vn>
2022-11-23 21:22:37 +0700

    exercise 4

---
+++ lesson_02/odd-even.js
@@ -0,0 +1,11 @@
+function evenOdd(number) {
+    if(number % 2 == 0) {
+        console.log(`${number} is even`);
+    } else if (number % 2 == 1){
+        console.log(`${number} is even`);
+    } else {
+        console.log("Invalid input");
+    }
+}
+
+evenOdd(39);
---
+++ lesson_02/random.js
@@ -0,0 +1,5 @@
+function random() {
+    return Math.round(Math.random() * 5) +3;
+}
+
+console.log(random());
