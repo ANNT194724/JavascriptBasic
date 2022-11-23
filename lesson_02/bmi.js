8850a3309eb01ddbfe4f42b3f1b24b9fa50bedc4
NGUYEN TRUONG AN <an.nt194724@sis.hust.edu.vn>
2022-11-23 19:33:53 +0700

    fix error

---
+++ lesson_02/bmi.js
@@ -0,0 +1,30 @@
+function bmi(w, h) {
+	return w/(h*h);   
+}
+
+function compare(bmi) {
+	switch (true) {
+		case bmi < 18.5:
+			console.log("Underweight");
+			break;
+		case bmi <25:
+			console.log("Normal weight");
+			break;
+		case bmi < 30:
+			console.log("Pre-obesity");
+			break;
+		case bmi < 35:
+			console.log("Obesity clas I");
+			break;
+		case bmi < 40:
+			console.log("Obesity clas II");
+			break;
+		default:
+			console.log("Obesity clas III");
+			break;
+		}
+	}
+
+var h = 1.76;
+var w = 88.5;
+compare(bmi(w, h));
