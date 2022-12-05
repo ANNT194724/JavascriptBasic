66a0fd203e556aa84b652ba6d507eec4a7658992
NGUYEN TRUONG AN <an.nt194724@sis.hust.edu.vn>
2022-12-05 20:59:58 +0700

    lesson 6

--- lesson_06/light.js
+++ lesson_06/light.js
@@ -1,11 +1,14 @@
 const button = document.querySelector('.lamp-button');
+let on = false;
 button.addEventListener("click", () => {
     var room = document.querySelector('.room');
-    if (room.style.backgroundColor === "white") {
+    if (on) {
         room.style.backgroundColor = "black";
         button.style.backgroundColor = "white";
+        on = false;
     } else {
         room.style.backgroundColor = "white";
         button.style.backgroundColor = "yellow";
+        on = true;
     }
 })
